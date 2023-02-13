ab9d4bc67b853db8078fa73aca0b531d04690898
Cauã Felipe <cauafelipe398@gmail.com>
2023-02-13 16:56:03 -0300

    Atualizando

---
+++ modulo/calcularTabuada.js
@@ -0,0 +1,16 @@
+/********************************** 
+ * Objetivo: Criar um app de tabuada
+ * Autor: Cauã Felipe
+ * Data: 13/02/2023
+ * Versão: 1.0
+ **********************************/
+
+const calcularTabuada = function (minimoMultiplicador, maximoMultiplicador, minimoMultiplicando, maximoMultiplicando ){
+    let miniMultiFixo = Number(minimoMultiplicador);
+    let maxiMulti = Number(maximoMultiplicador);
+    let valorMinMulti = Number(minimoMultiplicando);
+    let valorMaxMulti = Number(maximoMultiplicando);
+
+
+
+}
