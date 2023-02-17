5a4e19b24cc8e7b7e3ce25f993b38ca4e88a19c4
Caua Felipe <cauafelipe398@gmail.com>
2023-02-16 22:13:39 -0300

    Finalizado

--- modulo/calcularTabuada.js
+++ modulo/calcularTabuada.js
@@ -10,7 +10,32 @@ const calcularTabuada = function (minimoMultiplicador, maximoMultiplicador, mini
     let maxiMulti = Number(maximoMultiplicador);
     let valorMinMulti = Number(minimoMultiplicando);
     let valorMaxMulti = Number(maximoMultiplicando);
+    let status = true;
+    let resultado;
 
+    if  ( isNaN ( valorMinMulti )  ||  isNaN ( valorMaxMulti )  ||  isNaN ( maximoMultiplicador )  ||  isNaN ( miniMultiFixo ) )  {
+        estado  =  falso ;
+    }  else  if  ( valorMinMulti  >  valorMaxMulti  ||  miniMultiFixo  >  maxiMulti )  {
+        estado  =  falso ;
+    }  else  if ( valorMinMulti  <  2  ||  valorMinMulti  >  100  ||  valorMaxMulti  <  2  ||  valorMaxMulti  >  100 ) {
+        estado  =  falso ;
+    }  else  if ( miniMultiFixo  <  1  ||  miniMultiFixo  >  50  ||  maxiMulti  <  1  ||  maxiMulti  >  50 ) {
+        estado  =  falso ;
+    } else {
+        while (!(valorMin > valorMax)) {
+            console.log('\n\nTabuada do ' + valorMin);
+            let minMulti = Number(minMultiplicador);
+            while (!(minMulti > maxMulti)) {
+                resultado = valorMin * minMulti;
+                console.log(valorMin + ' X ' + minMulti + ' = ' + resultado);
+                minMulti++;
+            }
+            valorMin++;
+        }
+    }
+    return status;
+};
 
-
+module.exports = {
+    calcularTabuada
 }
